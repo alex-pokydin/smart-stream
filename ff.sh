64103169906b4bf1411159bc6ef505fea9e01149
Alex <hunterpav@gmail.com>
2022-04-25 07:59:06 +0300

    pgrep improvements

--- ff.sh
+++ ff.sh
@@ -9,38 +9,48 @@ ffmpeg_min_cpu=6
 ffmpeg_max_cpu=30
 flag=0
 
+SYSLOG() {
+  echo $1
+  logger -t "STREAM" $1
+}
+
 log_func() {
-        log_msg=$(echo "[$cam_ip] $1")
-        echo $log_msg
-        logger -t "STREAM" $log_msg
+    SYSLOG "[$cam_ip] $1"
 }
 
 if [ "$#" -eq 0 ]
 then
     file="$(dirname $0)/streams.txt"
-    logger -t "STREAM" "Reading streams from: ${file}"
+    SYSLOG "Reading streams from: ${file}"
 
     while IFS='' read -r LinefromFile || [[ -n "${LinefromFile}" ]]; do
-      logger -t "STREAM" "Check: ${LinefromFile}"
+      SYSLOG "Check: ${LinefromFile}"
       set $LinefromFile
-      ffpid=$(ps -ef | grep ffmpeg | grep $1 | grep -v grep | awk '{print $2}')
-      logger -t "STREAM" "PID = $ffpid"
       
-      if test -z "$ffpid"
+      # check ffmpeg instance count
+      count=$(pgrep -fc "ffmpeg.*$1.*$2")
+      if [ $count -gt 1 ]
       then
-        logger -t "STREAM" "ffmpeg starting: $0 ${LinefromFile}"
-        eval "$0 ${LinefromFile}" &>/dev/null & disown;
+        killed=$(pkill -fc "$1.*$2")
+        SYSLOG "Too much processes, killed: $killed"
+      fi
+
+      # check if ffmpeg is running
+      pid=$(pgrep -f "ffmpeg.*$1.*$2")
+      SYSLOG "PID = $pid"
+      if test -z "$pid"
+      then
+        SYSLOG "ffmpeg starting: $0 ${LinefromFile}"
+        eval "$0 ${LinefromFile}> /dev/null 2>&1 < /dev/null &"
       else
-        cpu=$(ps -o %cpu -p $ffpid| grep -v CPU)
+        cpu=$(ps -o %cpu -p $pid| grep -v CPU)
         cur_cpu=${cpu%.*}
-        txt=$(echo "[pid: $ffpid, cpu: $cur_cpu ( min: $ffmpeg_min_cpu, max: $ffmpeg_max_cpu )]")
-        logger -t "STREAM" "ffmpeg running... $txt"
+        txt=$(echo "[pid: $pid, cpu: $cur_cpu ( min: $ffmpeg_min_cpu, max: $ffmpeg_max_cpu )]")
+        SYSLOG "ffmpeg running... $txt"
         if [ $cur_cpu -lt $ffmpeg_min_cpu ] || [ $cur_cpu -gt $ffmpeg_max_cpu ]
         then
-            logger -t "STREAM" "ffmpeg PROBLEM!"
-            ps -ef | grep ffmpeg | grep $1 | grep -v grep | awk '{print $2}' | xargs -r kill -9
-            #sleep 3
-            #eval "$0 ${LinefromFile}" &>/dev/null & disown;
+            killed=$(pkill -fc "ffmpeg.*$1.*$2")
+            SYSLOG "ffmpeg PROBLEM! killed: $killed"
         fi
       fi
     done < "$file"
@@ -48,68 +58,35 @@ then
     exit 0
 fi
 
-
+# starting ffmpeg loop
 cam_ip=$1
 youtube_key=$2
-
-
 rtsp=$(echo "rtsp://$cam_ip:554/user=admin_password=tlJwpbo6_channel=1_stream=0.sdp?real_stream")
 if [ "$#" -eq 3 ]
 then
     rtsp=$(echo $3)
 fi
 
-
-ff_func() {
-
-    ps -ef | grep ffmpeg | grep $1 | grep -v grep | awk '{print $2}' | xargs -r kill -9
-    log_func "Start ffmpeg: $rtsp, $youtube_key"
-
-#        -hide_banner \
-#        -stats \
-#        -loglevel error \
-#        -stimeout 5000000 \
-
-    ffmpeg \
-        -rtsp_transport tcp \
-	      -fflags +genpts \
-	      -re \
-        -i "$rtsp" \
-        -f lavfi \
-        -i anullsrc \
-        -use_wallclock_as_timestamps 1 \
-        -c:v copy \
-        -c:a aac \
-	      -threads 1 \
-        -f flv "rtmp://a.rtmp.youtube.com/live2/$youtube_key"
-
-    sleep 3
-}
-
 while true
 do
+  killed=$(pkill -fc "ffmpeg.*$1.*$2")
+  log_func "Killing ffmpeg instances! killed: $killed"
+  log_func "Start ffmpeg: $rtsp, $youtube_key"
 
-log_func "Start test ffmpeg"
-
-ffpid=$(ps -ef | grep ffmpeg | grep $cam_ip | grep -v grep | awk '{print $2}')
-cpu=$(ps -o %cpu -p $ffpid| grep -v CPU)
-cur_cpu=${cpu%.*}
-#log_func "curr cpu: $cur_cpu, config: $ffmpeg_min_cpu"
-#diff=$(($cur_cpu - $ffmpeg_min_cpu))
-#log_func "diff: $diff"
-txt=$(echo "[pid: $ffpid, cpu: $cur_cpu, min: $ffmpeg_min_cpu, max: $ffmpeg_max_cpu]")
-
-#if [ $cur_cpu -gt $ffmpeg_min_cpu ] && [ $cur_cpu -lt $ffmpeg_max_cpu ]
-#then
-#        log_func "ffmpeg RUNNING! $txt"
-#        flag=0
-#else
-#        log_func "ffmpeg PROBLEM! $txt"
-#        ps -ef | grep ffmpeg | grep $cam_ip | grep -v grep | awk '{print $2}' | xargs -r kill -9
-#        sleep 3
-        ff_func
-#fi
-
+  ffmpeg \
+      -rtsp_transport tcp \
+      -fflags +genpts \
+      -re \
+      -i "$rtsp" \
+      -f lavfi \
+      -i anullsrc \
+      -use_wallclock_as_timestamps 1 \
+      -c:v copy \
+      -c:a aac \
+      -threads 1 \
+      -f flv "rtmp://a.rtmp.youtube.com/live2/$youtube_key"
+  
+  sleep 1
 done
 
 exit 0
